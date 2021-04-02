15afd7cfc71759bea4318463ec611ce4bd6e8262
shubhamgaur12 <shubhamgaur1994@gmail.com>
2021-04-02 20:43:54 +0530

    checkpoint 11

--- scripts/common.js
+++ scripts/common.js
@@ -3,34 +3,13 @@ document.getElementById('header').innerHTML=header;
 const footer=  `<div id="contact"><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-backdrop="false" data-target="#contact-modal">Contact Us</button><div class="modal fade" id="contact-modal" tabindex="-1" role="dialog" aria-labelledby="contact-modal-label" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="contact-modal-label">Get in touch</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><form><p>Thank you for reaching out!!!<br>Please enter you email and we will get back to you.</p><label for="email">Email: </label><input type="text" id="email" name="email" placeholder="Enter your email id" required></form></div><div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal">Sumbit</button></div></div></div></div></div><div id="copyright-text">© 2020 ROOM SEARCH PVT. LTD.</div><div id="social-media-images"><a href="https://www.facebook.com" target="_blank"><img src="assests/images/facebook.png" class="social-media-image"></a><a href="https://www.instagram.com" target="_blank"><img src="assests/images/instagram.png" class="social-media-image"></a><a href="https://twitter.com" target="_blank"><img src="assests/images/twitter.png" class="social-media-image"></a></div>`
 document.getElementById('footer').innerHTML=footer;
 var Login= function () {
-    var self = this;
-    self.showLoginButton = ko.observable(true);
-    self.showLogoutButton = ko.observable(false);
-    self.login = function (callback) {
-        var loginData = {
-            grant_type: 'password',
-            username: self.loginUserName(),
-            password: self.loginPassword()
-        };
-        $.ajax({
-            type: 'POST',
-            url: '../API/Token',
-            data: loginData
-        }).done(function (data) {
-            sessionStorage.setItem(tokenKey, data.access_token);
-            localStorage.setItem("userID", self.loginUserName());
-            self.authenticate();
-            self.showLogoutButton(true);
-            self.showLoginButton(false);
-            if (callback && typeof callback == "function")
-                callback();
-    
-            self.redirect('MyPage');
-        }).fail(showError);
+    if(username!=null & passowrd!=null){
+      document.getElementById("buttonLogin").style.visibility = null;
+      document.getElementById("buttonLogout").style.visibility = "hidden";
+    }
+    else{
+      document.getElementById("buttonLogin").style.visibility = "hidden";
+      document.getElementById("buttonLogout").style.visibility = null;
     }
     
-    self.logout = function () {
-        sessionStorage.removeItem(tokenKey)
-        self.showLoginButton(true);
-        self.showLogoutButton(false);
-     }  }
+  }
